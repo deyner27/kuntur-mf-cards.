cc4258a41bc5b794643c2bb1cb4ff5f521cc139d
Gianmarco09 <gianmarcosalomonmeonomorocho@gmail.com>
2024-03-19 21:34:11 -0500

    subir avance

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  exports: [MatIconModule],
+  exports: [MatIconModule, MatCardModule],
 })
 export class MaterialModule { }
